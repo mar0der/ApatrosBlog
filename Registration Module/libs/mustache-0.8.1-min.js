2b019a8dd548925d9448826a26d394ea08d2ffc7
Lyubomir Panov <lyubomir.panov@gmail.com>
2015-04-20 22:46:06 +0300

    Registration Module

---
+++ Registration Module/libs/mustache-0.8.1-min.js
@@ -0,0 +1,310 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>mustachejs - cdnjs.com - the missing cdn for javascript and css</title>
+  <meta property="og:title" content="mustachejs - cdnjs.com - the missing cdn for javascript and css" />
+  <meta property="og:image" content="/img/logotext.png" />
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <meta name="description" content="Logic-less {{mustache}} templates with JavaScript">
+  <link rel="search" type="application/opensearchdescription+xml" href="/opensearch.xml" title="cdnjs">
+  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.2/css/toastr.min.css">
+  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.1.1-1/css/united/bootstrap.min.css">
+  <link rel="stylesheet" href="/css/theme.css">
+  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.1.1/gh-fork-ribbon.min.css" />
+  <!--[if lt IE 9]>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.1.1/gh-fork-ribbon.ie.min.css" />
+  <![endif]-->
+</head>
+
+<body>
+  <div class="github-fork-ribbon-wrapper right">
+    <div class="github-fork-ribbon">
+      <a href="https://github.com/cdnjs/cdnjs">Help us on GitHub!</a>
+    </div>
+  </div>
+  <div class="container">
+
+    <div class="row">
+      <div class="col-md-8">
+        <div class="navbar navbar-default">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/">cdnjs</a>
+          </div>
+          <div class="navbar-collapse collapse navbar-responsive-collapse">
+            <ul class="nav navbar-nav">
+              <li class=""><a href="http://github.com/cdnjs/cdnjs" class="translateit">Add a library</a></li>
+              <li class=""><a href="/about" class="translateit">About</a></li>
+
+            </ul>
+            <ul class="nav navbar-nav">
+              <li class=""><a href="https://gitter.im/cdnjs/cdnjs" class="translateit">Chat</a></li>
+
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a style="display: none;" class="login translateit" href="/login" >Login</a>
+              <li><a style="display: none;" class="logout translateit" href="/logout" >Logout</a>
+              <li><a style="display: none;" class="register translateit" href="/register" >Register</a>
+              <li><a style="display: none;" class="profile translateit" href="/profile" >Profile</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <p>
+      <span class="translateit">Everyone loves the&nbsp;</span><a target="_blank" href="http://code.google.com/apis/libraries/devguide.html" class="translateit">Google CDN</a><span class="translateit">&nbsp;right? Even&nbsp;</span><a href="http://www.asp.net/ajaxlibrary/cdn.ashx" target="_blank" class="translateit">Microsoft runs their own CDN</a>.
+      <br><span class="translateit">The problem is, they only host the most popular libraries.</span>
+      <br><span class="translateit">We host it all - JavaScript, CSS, SWF, images, etc!</span>
+      <br><span class="translateit">Join us now on GitHub!</span><br>
+      <iframe src="https://ghbtns.com/github-btn.html?user=cdnjs&repo=cdnjs&type=watch&count=true&v=2" frameborder="0" scrolling="0" width="105px" height="20px"></iframe>
+      <iframe src="https://ghbtns.com/github-btn.html?user=cdnjs&repo=cdnjs&type=star&count=true" frameborder="0" scrolling="0" width="105px" height="20px"></iframe>
+      <iframe src="https://ghbtns.com/github-btn.html?user=cdnjs&repo=cdnjs&type=fork&count=true" frameborder="0" scrolling="0" width="105px" height="20px"></iframe>
+    </p>
+      </div>
+      <div class="col-md-4">
+        <script async type="text/javascript" src="https://cdn.carbonads.com/carbon.js?zoneid=1673&serve=C6AILKT&placement=cdnjscom" id="_carbonads_js"></script>
+ 
+    </div>
+
+
+
+
+    <div class="clear"></div>
+    <div class="page">
+      <div class="row library-container">
+  <div class="col-md-12">
+    <h1 style="margin-top: 00px;">mustache.js</h1>
+
+    <p><a href="https:&#x2F;&#x2F;github.com&#x2F;janl&#x2F;mustache.js">https:&#x2F;&#x2F;github.com&#x2F;janl&#x2F;mustache.js</a>
+      <p class="translateit">Logic-less {{mustache}} templates with JavaScript</p>
+      <em class="translateit">templates, templating, mustache, template, ejs</em>
+      <br />
+      <br />
+      <br />
+      <select class="form-control version-selector" style="float: right;">
+          <option value="0.8.1" selected=&quot;selected&quot;>0.8.1</option>
+          <option value="0.7.2" >0.7.2</option>
+          <option value="0.7.0" >0.7.0</option>
+          <option value="0.5.0-dev" >0.5.0-dev</option>
+          <option value="0.4.2" >0.4.2</option>
+          <option value="0.3.0" >0.3.0</option>
+      </select>
+      <div style="clear: both;"></div>
+      <br />
+      <div data-library-version="0.8.1" class="assets panel panel-primary active" style="display: none;">
+        <div class="panel-heading">
+          <div class="panel-title" class="translateit">Version 0.8.1</div>
+        </div>
+
+        <div class="panel-body">
+
+          <table style="margin-top: 10px;" cellpadding="0" cellspacing="0" border="0" class="table table-striped" id="example">
+
+            <tbody>
+              <tr class="library">
+                <td class="library-column"  data-lib-name="mustache.js" style="position: relative;">
+                  <p class='library-url'>https://cdnjs.cloudflare.com/ajax/libs/mustache.js/0.8.1/mustache.js</p>
+                </td>
+              </tr>
+              <tr class="library">
+                <td class="library-column"  data-lib-name="mustache.js" style="position: relative;">
+                  <p class='library-url'>https://cdnjs.cloudflare.com/ajax/libs/mustache.js/0.8.1/mustache.min.js</p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <div data-library-version="0.7.2" class="assets panel panel-primary " style="display: none;">
+        <div class="panel-heading">
+          <div class="panel-title" class="translateit">Version 0.7.2</div>
+        </div>
+
+        <div class="panel-body">
+
+          <table style="margin-top: 10px;" cellpadding="0" cellspacing="0" border="0" class="table table-striped" id="example">
+
+            <tbody>
+              <tr class="library">
+                <td class="library-column"  data-lib-name="mustache.js" style="position: relative;">
+                  <p class='library-url'>https://cdnjs.cloudflare.com/ajax/libs/mustache.js/0.7.2/mustache.js</p>
+                </td>
+              </tr>
+              <tr class="library">
+                <td class="library-column"  data-lib-name="mustache.js" style="position: relative;">
+                  <p class='library-url'>https://cdnjs.cloudflare.com/ajax/libs/mustache.js/0.7.2/mustache.min.js</p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <div data-library-version="0.7.0" class="assets panel panel-primary " style="display: none;">
+        <div class="panel-heading">
+          <div class="panel-title" class="translateit">Version 0.7.0</div>
+        </div>
+
+        <div class="panel-body">
+
+          <table style="margin-top: 10px;" cellpadding="0" cellspacing="0" border="0" class="table table-striped" id="example">
+
+            <tbody>
+              <tr class="library">
+                <td class="library-column"  data-lib-name="mustache.js" style="position: relative;">
+                  <p class='library-url'>https://cdnjs.cloudflare.com/ajax/libs/mustache.js/0.7.0/mustache.min.js</p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <div data-library-version="0.5.0-dev" class="assets panel panel-primary " style="display: none;">
+        <div class="panel-heading">
+          <div class="panel-title" class="translateit">Version 0.5.0-dev</div>
+        </div>
+
+        <div class="panel-body">
+
+          <table style="margin-top: 10px;" cellpadding="0" cellspacing="0" border="0" class="table table-striped" id="example">
+
+            <tbody>
+              <tr class="library">
+                <td class="library-column"  data-lib-name="mustache.js" style="position: relative;">
+                  <p class='library-url'>https://cdnjs.cloudflare.com/ajax/libs/mustache.js/0.5.0-dev/mustache.js</p>
+                </td>
+              </tr>
+              <tr class="library">
+                <td class="library-column"  data-lib-name="mustache.js" style="position: relative;">
+                  <p class='library-url'>https://cdnjs.cloudflare.com/ajax/libs/mustache.js/0.5.0-dev/mustache.min.js</p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <div data-library-version="0.4.2" class="assets panel panel-primary " style="display: none;">
+        <div class="panel-heading">
+          <div class="panel-title" class="translateit">Version 0.4.2</div>
+        </div>
+
+        <div class="panel-body">
+
+          <table style="margin-top: 10px;" cellpadding="0" cellspacing="0" border="0" class="table table-striped" id="example">
+
+            <tbody>
+              <tr class="library">
+                <td class="library-column"  data-lib-name="mustache.js" style="position: relative;">
+                  <p class='library-url'>https://cdnjs.cloudflare.com/ajax/libs/mustache.js/0.4.2/mustache.min.js</p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <div data-library-version="0.3.0" class="assets panel panel-primary " style="display: none;">
+        <div class="panel-heading">
+          <div class="panel-title" class="translateit">Version 0.3.0</div>
+        </div>
+
+        <div class="panel-body">
+
+          <table style="margin-top: 10px;" cellpadding="0" cellspacing="0" border="0" class="table table-striped" id="example">
+
+            <tbody>
+              <tr class="library">
+                <td class="library-column"  data-lib-name="mustache.js" style="position: relative;">
+                  <p class='library-url'>https://cdnjs.cloudflare.com/ajax/libs/mustache.js/0.3.0/mustache.min.js</p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+
+      <!--
+      <div id="disqus_thread"></div>
+      <script type="text/javascript">
+          /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+          var disqus_shortname = 'cdnjsweb'; // required: replace example with your forum shortname
+          var disqus_identifier = '/libraries/mustache.js';
+          var disqus_title = 'mustache.js';
+          var disqus_url = 'http://cdnjs.com/libraries/mustache.js';
+          /* * * DON'T EDIT BELOW THIS LINE * * */
+          (function() {
+              var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+              dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+              (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+          })();
+      </script>
+      <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+    -->
+  </div>
+</div>
+
+    </div>
+  </div>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/handlebars.js/1.0.0/handlebars.min.js"></script>
+  <script src="https://app.userapp.io/js/userapp.client.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.1/js/bootstrap-dropdown.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/zeroclipboard/2.0.0-beta.5/ZeroClipboard.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.2/js/toastr.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-timeago/1.4.0/jquery.timeago.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/algoliasearch/2.8.0/algoliasearch.min.js"></script>
+  <script src="/js/kaka.js"></script>
+  <script src="/js/main.js"></script>
+
+
+    <script src="//static.getclicky.com/js"></script>
+    <script type="text/javascript">try{clicky.init(66376315);}catch(err){}</script>
+
+    <noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/66376315ns.gif" /></p></noscript>
+
+
+
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    
+      ga('create', 'UA-44291190-1', 'auto');
+      ga('require', 'linkid', 'linkid.js');
+      ga('require', 'displayfeatures');
+      ga('send', 'pageview');
+    
+    </script>
+    <script type="text/javascript">
+    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+    var disqus_shortname = 'cdnjsweb'; // required: replace example with your forum shortname
+
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function () {
+    var s = document.createElement('script'); s.async = true;
+    s.type = 'text/javascript';
+    s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+    (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+    }());
+    </script>
+</body>
+
+</html>
