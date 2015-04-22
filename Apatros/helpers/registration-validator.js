c1f1470e3f224125da8ec22d9714b33df1a4bc7d
Petar Petkov <petar.b.petkov@gmail.com>
2015-04-23 00:02:35 +0300

    login improved

--- Apatros/helpers/registration-validator.js
+++ Apatros/helpers/registration-validator.js
@@ -7,70 +7,70 @@ define([], function () {
         inputType = $(input).attr('type');
 
         switch (inputType) {
-            case('password'):
-            {
-                if (validInputLength(input)) {
-                    var passwordId = input.attr('id');
+            case ('password'):
+                {
+                    if (validInputLength(input)) {
+                        var passwordId = input.attr('id');
 
-                    switch (passwordId) {
-                        case('register-password'):
-                        {
-                            var password = input.val();
-                            var passwordToMatch = $('#repeat-password');
-                            var passwordMatch = validatePasswordMatch(password, passwordToMatch.val());
+                        switch (passwordId) {
+                            case ('register-password'):
+                                {
+                                    var password = input.val();
+                                    var passwordToMatch = $('#repeat-password');
+                                    var passwordMatch = validatePasswordMatch(password, passwordToMatch.val());
 
-                            if (passwordToMatch && passwordMatch) {
-                                passValidation(input);
-                                passValidation(passwordToMatch);
-                            } else if (passwordToMatch) {
-                                passValidation(input);
-                                failValidation(passwordToMatch);
-                            } else {
-                                failValidation(input);
-                                failValidation(passwordToMatch);
-                            }
-                            break;
+                                    if (passwordToMatch && passwordMatch) {
+                                        passValidation(input);
+                                        passValidation(passwordToMatch);
+                                    } else if (passwordToMatch) {
+                                        passValidation(input);
+                                        failValidation(passwordToMatch);
+                                    } else {
+                                        failValidation(input);
+                                        failValidation(passwordToMatch);
+                                    }
+                                    break;
+                                }
+
+                            case ('repeat-password'):
+                                {
+                                    var password = input.val();
+                                    var passwordToMatch = $('#register-password');
+                                    var passwordMatch = validatePasswordMatch(password, passwordToMatch.val());
+
+                                    if (passwordToMatch && passwordMatch) {
+                                        passValidation(input);
+                                        passValidation(passwordToMatch);
+                                    } else {
+                                        failValidation(input);
+                                    }
+                                    break;
+                                }
                         }
 
-                        case('repeat-password'):
-                        {
-                            var password = input.val();
-                            var passwordToMatch = $('#register-password');
-                            var passwordMatch = validatePasswordMatch(password, passwordToMatch.val());
-
-                            if (passwordToMatch && passwordMatch) {
-                                passValidation(input);
-                                passValidation(passwordToMatch);
-                            } else {
-                                failValidation(input);
-                            }
-                            break;
-                        }
+                    } else {
+                        failValidation(input);
+                        failValidation($('#repeat-password'));
                     }
-
-                } else {
-                    failValidation(input);
-                    failValidation($('#repeat-password'));
+                    break;
                 }
-                break;
-            }
 
-            case('email'):
-            {
-                if (validInputLength(input)) {
-                    if (validEmail(input)) {
-                        passValidation(input);
-                    }
-                    else {
+            case ('email'):
+                {
+                    if (validInputLength(input)) {
+                        if (validEmail(input)) {
+                            passValidation(input);
+                        }
+                        else {
+                            failValidation(input);
+                        }
+                    } else {
                         failValidation(input);
                     }
-                } else {
-                    failValidation(input);
+                    break;
                 }
-                break;
-            }
 
-            default :
+            default:
                 if (validInputLength(input)) {
                     passValidation(input);
                 } else {
@@ -81,7 +81,7 @@ define([], function () {
     }
 
     function validInputLength(input) {
-        return input.val().length > 0;
+        return input.val().trim().length > 0;
     }
 
     function validatePasswordMatch(password, passwordToMatch) {
@@ -92,7 +92,6 @@ define([], function () {
         var emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
         return emailPattern.test(email.val());
     }
-
 
     function passValidation(input) {
         input.next('.validation').addClass('passed').html('&#10004;');
